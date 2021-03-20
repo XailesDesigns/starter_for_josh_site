4ca930e89f2cf5adc301e13300e6fd6eaa162d09
XailesDesigns <79856750+XailesDesigns@users.noreply.github.com>
2021-03-21 05:28:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,107 +1,73 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>JOSH WHITKIN</title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="responsive.css" />
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,800&display=swap" rel="stylesheet">
-</head>
-
-<body>
-    <header class="container-fluid herobg">
-        <nav class="navbar">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavbar">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">HOME</a></li>
-                        <li><a href="#">PROJECTS</a></li>
-                        <li><a href="#">PUBLICATIONS</a></li>
-                        <li><a href="#">CV</a></li>
-                        <li><a href="#">CONTACTS</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!--div for the main image the stretches to the edge of the page -->
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
         <div class="container">
-            <!-- div that holds the content in the middle of the page-->
-            <h1>Games Design for the Future</h1>
-            <!--the main tag line -->
-        </div>
-        <!--container-->
-    </header>
-    <section class="container-fluid aboutbg text-center">
+			
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+     <section class="container-fluid aboutbg text-center">
         <div class="container">
-            <h2> GAMES &amp; LEVEL DESIGNER </h2>
-            <p class="particle">...and particle wizard</p>
-            <p class="about-text">My name is josh Whitkin. I’m a professional with more than 7 years of industry experience in Games Design, Level design, Scripting and Visual Effects. What makes me passionate about games is the combination of technology and creativity, and how multiple disciplines come together to create an interactive experience. Whether I'm designing for puzzle games, 2D platformers or 3D environments. I always tell a story.</p>
-            <div class="blocker"></div>
-        </div>
+            
+        
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=2');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>    
+    	
+ <div class="blocker"></div>
+            
+      </div><!--  container-->
     </section>
-    <section class="container-fluid articlesbg">
+
+
+      </div><!--  container-->
+    </section>
+
+
+
+
+    <section class="container-fluid ">
         <div class="container">
-            <div class="row">
-                <!--a row that gives us access to the bootstrap columns-->
-                <div class="article-title text-center">
+            <div class="row"><!--  a row that gives us access to the BS columns-->
+                       <div class="article-title text-center">
                     <h3>LATEST ARTICLES</h3>
                 </div>
-                <div class="col-md-4 article-cent"> <img class="img-responsive" alt="portrait" src="images/portrait1.jpg">
+                <div class="col-md-4 article-cent">
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ portrait.jpg">
                     <h4>A PORTRAIT</h4>
-                    <p>12 Dececmeber 2020</p>
-                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                    <p class="readmore">CONTINUE READING</p>
+                                       <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                               <p class="readmore">CONTINUE READING</p>
                 </div>
-                <div class="col-md-4 article-cent"> <img class="img-responsive" alt="portait" src="images/portrait2.jpg">
+                <div class="col-md-4 article-cent">
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
                     <h4>A MODEL</h4>
-                    <p class="date">12 December 2020</p>
-                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                    <p class="readmore">CONTINUE READING</p>
+                                        <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                                   <p class="readmore">CONTINUE READING</p>
                 </div>
-                <div class="col-md-4 article-cent"> <img class="img-responsive" alt="portait" src="images/portrait3.jpg">
-                    <h4>GAMES DESIGN</h4>
-                    <p class="date">12 December 2020</p>
-                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts. </p>
-                    <p class="readmore">CONTINUE READING</p>
-                </div>
-            </div>
-        </div>
-    </section>
-    <footer class="container-fluid">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 text-center footer-content">
-                    <p>JOSH WHITKIN</p>
-                    <img src="images/tumblr.jpg" alt="tumblr"> <img src="images/rss.jpg" alt="rss">
-                </div>
-                <div class="col-md-6 text-center footer-content">
-                    <p>Terms and Conditions</p>
-                    <img src="images/linkedin.jpg" alt="linkedin"> <img src="images/fb.jpg" alt="facebook">
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-</body>
+                <div class="col-md-4 article-cent">
+                                        <img class="img-responsive" alt=“portait” src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
 
-</html>
+                    <h4>A GAMES DESIGN</h4>
+                                        <p class="date">12 December 2020</p>
+                    <p>STS has never been shy of seeking new terrains of investigation. More and more STS s cholars are starting to explore and intervene in the arts. This object of study brings new challenges and opportunities that we want to explore in this session. We would like to gather first of all simply new kinds of knowledge arising from STS study of the arts.
+                    </p>
+                                      <p class="readmore">CONTINUE READING</p>
+                </div>
+                
+                    </div><!-- row-->
+            </div><!-- container-->
+        
+    </section><!-- container-fluid-->
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
